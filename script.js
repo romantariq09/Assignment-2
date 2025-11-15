45ca82649f9edb4f5b539734de620bb66fce6df2
Romaan Tariq <tm23834@gmail.com>
2025-11-15 02:09:39 -0500

    define the structure by adding constructor

---
+++ script.js
@@ -0,0 +1,10 @@
+//  defines the structure of each smoothie object
+class Smoothie {
+    constructor(size, base, ingredients, sweetness) {
+        this.size = size;
+        this.base = base;
+        this.ingredients = ingredients;
+        this.sweetness = sweetness;
+    }
+}
+
