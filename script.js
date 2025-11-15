74c509bcfdf3f7e25468d2558d7e3ca6e1593663
Romaan Tariq <tm23834@gmail.com>
2025-11-15 02:13:02 -0500

    added description method

--- script.js
+++ script.js
@@ -6,5 +6,16 @@ class Smoothie {
         this.ingredients = ingredients;
         this.sweetness = sweetness;
     }
-}
 
+
+// description of the smoothie
+    describe() {
+        return `
+            <h2>Your Smoothie 🍹</h2>
+            <p><strong>Size:</strong> ${this.size}</p>
+            <p><strong>Base:</strong> ${this.base}</p>
+            <p><strong>Ingredients:</strong> ${this.ingredients.join(", ")}</p>
+            <p><strong>Sweetness:</strong> ${this.sweetness}</p>
+        `;
+    }
+}
